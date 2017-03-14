c6248b971ed40755e95d36b1f13df4c10b7647c0
Enrique Worwa <enrique.worwa@creode.co.uk>
2017-03-14 10:26:55 +0000

    custom module updated

--- html/app/code/local/Creode/TestModule/Model/Observer.php
+++ html/app/code/local/Creode/TestModule/Model/Observer.php
@@ -6,18 +6,29 @@ class Creode_TestModule_Model_Observer
 			{
 				$quote = $observer->getEvent()->getQuote();
 	            $item = $observer->getQuoteItem();
-	            $product_id=$item->getProductId();
-	            $product=Mage::getModel('catalog/product')->load($product_id);
+	            $product_id = $item->getProductId();
+	            $product = Mage::getModel('catalog/product')->load($product_id);
 
-	            echo '<pre>';
-	            print_r($product->getName());
-	            print_r($product->getCategoriess());
-	            exit;
+	            $productCategories = $product->getCategoryIds();
 
-				// Check for the product category and the setting catgory, if are the same apply the discount according to the other setting, and print a message
+	            $increasePriceCategoryId = Mage::getStoreConfig('price_handler_section/prince_handler_group/price_increment_category');
+	            $increasePriceAmount = Mage::getStoreConfig('price_handler_section/prince_handler_group/price_increment');
 
+	            // print_r($discountCategoryId);
 
+	            foreach ($productCategories as $id => $categories) {
+	            	if ($id == $discountCategoryId) {
+	            		$currentPrice = $product->getPrice();
+	            		$product->setPrice($currentPrice + $increasePriceAmount);
+	            		$product->save();
+	            		break;
+	            	}
+	            }
 
+	            // echo "<pre>";
+	            // print_r($product->getPrice());
+	            // exit;
+				
 			}
 		
 }
