37d1afb89011d30be6afb2c7dc8a57a50aa6b469
Enrique Worwa <enrique.worwa@creode.co.uk>
2017-03-28 09:43:17 +0100

    Custom shipping method module

---
+++ html/app/code/local/Creode/CustomShippingMethod/sql/categoryattribute1490178134_setup/mysql4-install-0.1.0.php
@@ -0,0 +1,53 @@
+<?php
+$installer = $this;
+$installer->startSetup();
+
+// Add Category attribute
+$installer->addAttribute("catalog_category", "apply_custom_shipping_method",  array(
+    "type"     => "int",
+    "backend"  => "",
+    "frontend" => "",
+    "label"    => "Eligible for custom shipping method",
+    "input"    => "select",
+    "class"    => "Shipping methods",
+    "source"   => "eav/entity_attribute_source_boolean",
+    "global"   => Mage_Catalog_Model_Resource_Eav_Attribute::SCOPE_STORE,
+    "visible"  => true,
+    "required" => true,
+    "user_defined"  => false,
+    "default" => "1",
+    "searchable" => false,
+    "filterable" => false,
+    "comparable" => false,
+    
+    "visible_on_front"  => false,
+    "unique"     => false,
+    "note"       => ""
+
+    ));
+
+// Add Product attribute
+$installer->addAttribute("catalog_product", "apply_custom_shipping_method",  array(
+    "type"     => "int",
+    "backend"  => "",
+    "frontend" => "",
+    "label"    => "Eligible for custom shipping method",
+    "input"    => "select",
+    "class"    => "Shipping methods",
+    "source"   => "eav/entity_attribute_source_boolean",
+    "global"   => Mage_Catalog_Model_Resource_Eav_Attribute::SCOPE_STORE,
+    "visible"  => true,
+    "required" => true,
+    "user_defined"  => false,
+    "default" => "1",
+    "searchable" => false,
+    "filterable" => false,
+    "comparable" => false,
+    
+    "visible_on_front"  => false,
+    "unique"     => false,
+    "note"       => ""
+
+    ));
+$installer->endSetup();
+	 
