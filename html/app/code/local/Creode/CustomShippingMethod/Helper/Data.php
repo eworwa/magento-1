3d0a182a875fcf9c7eebdc2c2b496e6895e7582d
Enrique Worwa <enrique.worwa@creode.co.uk>
2017-03-30 10:29:54 +0100

    Custom Shipping method logic implemented

--- html/app/code/local/Creode/CustomShippingMethod/Helper/Data.php
+++ html/app/code/local/Creode/CustomShippingMethod/Helper/Data.php
@@ -1,5 +1,11 @@
 <?php
 class Creode_CustomShippingMethod_Helper_Data extends Mage_Core_Helper_Abstract
 {
+	function getCategoriesIdWithCustomShippingMethod() {
+		$categoriesId = array(1,2,3,4,5,6,7);
+
+
+		return $categoriesId;
+	}
 }
 	 
--- html/app/code/local/Creode/CustomShippingMethod/Model/Shipping/Shipping.php
+++ html/app/code/local/Creode/CustomShippingMethod/Model/Shipping/Shipping.php
@@ -4,8 +4,52 @@ class Creode_CustomShippingMethod_Model_Shipping_Shipping extends Mage_Shipping_
 {
     public function collectCarrierRates($carrierCode, $request)
     {
-        echo "here";
-        die;
+        if ($carrierCode === 'customshippingmethod') {
+            $isElegible = true;
+
+            // @todo get real values from parameter
+            $helper = Mage::helper('customshippingmethod');
+            $categoriesCustomShippingMethod = $helper->getCategoriesIdWithCustomShippingMethod();
+
+            // echo "<pre>";
+            // print_r($categoriesCustomShippingMethod);
+
+            $cart = Mage::getModel('checkout/cart')->getQuote();
+
+            foreach ($cart->getAllItems() as $item) {
+                
+                // We need to instantiate the product in order to get the custom attributes
+                $product = Mage::getModel('catalog/product')->load($item->getProduct()->getId());
+                $productIsCustomShippingMethod = $product->getData('apply_custom_shipping_method');
+                $productCategories = $product->getCategoryIds();
+
+                // echo "***" . $productIsCustomShippingMethod . "***";
+                // die;
+                
+                // echo "<pre>";
+                // print_r($productCategories);
+                
+
+                if(!$productIsCustomShippingMethod) {
+                    // echo "product not shippable with custom shipping method";
+                    // There is at least one product not shippable with Custom Shipping Method
+                    return $this;
+                }
+
+                if(count(array_intersect($productCategories, $productIsCustomShippingMethod)) < 0) {
+                    // echo "category not shippable with custom shipping method";
+                    // There is at least one product with a category not shippable with Custom Shipping Method
+                    return $this;
+                }
+            }
+
+            // code never reached
+            if(!$isElegible) {
+                return $this;
+            } else {
+                return parent::collectCarrierRates($carrierCode, $request);
+            }
+        }
         
         return parent::collectCarrierRates($carrierCode, $request);
     }
