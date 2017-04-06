1ffc0a68d629a7dddb115300bb660ae79b71a77b
Enrique Worwa <enrique.worwa@creode.co.uk>
2017-04-06 10:07:16 +0100

    new widget homepage bottom text

---
+++ html/app/code/local/Creode/Homepage/Block/Homepagebottomtext.php
@@ -0,0 +1,26 @@
+<?php
+class Creode_Homepage_Block_Homepagebottomtext
+    extends Mage_Core_Block_Template
+    implements Mage_Widget_Block_Interface
+{
+
+    protected function _toHtml()
+    {
+        // $text = $this->getData('text');
+        // die;
+        
+        $text = "aqui";
+        
+		$html ='';
+        // $html .= 'homepagebannerslide parameter1 = '.$this->getData('parameter1').'<br/>';
+        // $html .= 'homepagebannerslide parameter2 = '.$this->getData('parameter2').'<br/>';
+        // $html .= 'homepagebannerslide parameter3 = '.$this->getData('parameter3').'<br/>';
+        // $html .= 'homepagebannerslide parameter4 = '.$this->getData('parameter4').'<br/>';
+        // $html .= 'homepagebannerslide parameter5 = '.$this->getData('parameter5').'<br/>';
+
+        // $html = $this->getData('title') . ' - ' . $this->getData('text');
+        $html .= '<div class="homepage-bottom-text"><p>' . $text . '</p></div>';
+        return $html;
+    }
+	
+}
